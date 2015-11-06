80c65feeb42f6f26e3eb7e9d262b6b1048d8f4f1
Jaewon Choi <jainersoer@ajou.ac.kr>
2015-11-06 19:27:44 +0900

    Initial commit

---
+++ cal.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+// youngmin
+int sub(int a, int b)
+{
+}
+
+// jjajang
+int mult(int a, int b)
+{
+}
+
+// $==S
+int div(int a, int b)
+{
+}
+
+// Jaewon Choi
+int main()
+{
+	return 0;
+}
