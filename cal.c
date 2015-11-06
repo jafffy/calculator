a185460441e82aea5a1e204973da1a58ccb0217d
Jaewon Choi <jainersoer@ajou.ac.kr>
2015-11-06 19:33:47 +0900

    add test and Makefile

--- cal.c
+++ cal.c
@@ -23,5 +23,10 @@ int div(int a, int b)
 // Jaewon Choi
 int main()
 {
+	printf("2 + 3 = %d\n", add(2, 3));
+	printf("2 - 3 = %d\n", sub(2, 3));
+	printf("2 * 3 = %d\n", mult(2, 3));
+	printf("2 / 3 = %d\n", div(2, 3));
+
 	return 0;
 }
